fc84357db3ae2b0693b6478d9b08362ce5cdada5
David Awad <davidawad@gmail.com>
2015-01-04 02:14:21 -0500

    downloads working!

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from flask import *
 from pymongo import MongoClient
-import sys,os,sendgrid,twilio, gridfs, pymongo  ##will add sendgrid and twilio functionality.
+import sys,os,sendgrid,twilio, gridfs, pymongo ##will add sendgrid and twilio functionality.
 from werkzeug import secure_filename
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -34,14 +34,22 @@ def read_file(output_location, room_number):
 		f.write(gfs.get(_id).read())
 	return gfs.get(_id).read()
 
+def search_file(spacenum):
+	db_conn = get_db()
+	gfs = gridfs.GridFS(db_conn)
+	_id = db_conn.fs.files.find_one(dict(room=room_number))
+	print _id
+
 @app.route('/upload',methods=['POST'])
 def upload():
 	#get the name of the uploaded file
-	file=request.files['file']
+	file = request.files['file']
 	#print "requested files"
-	space=request.form['space']
+	space = request.form['space']
 	# if the file exists make it secure
 	if file and space: #if the file exists
+		# search to see if number is taken
+		search_file(space)
 		#make the file same, remove unssopurted chars
 		filename=secure_filename(file.filename)
 		#move the file to our uploads folder
@@ -58,10 +66,11 @@ def upload():
 
 @app.route('/upload/<spacenum>', methods=['GET'])
 def download(spacenum):
-	unSecurefilename = read_file(app.config['UPLOAD_FOLDER'] ,spacenum)
-	return send_from_directory(app.config['UPLOAD_FOLDER'], str(spacenum)  )
-	#return render_template('index.html' , spacenum = spacenum)
-
+	unSecurefilename = read_file(app.config['UPLOAD_FOLDER'] ,spacenum )
+	render_template('index.html' , spacenum = spacenum)
+	return send_from_directory(app.config['UPLOAD_FOLDER'], str(spacenum) )
+	#remove from disk after file is given to user. use new thread to unlink??
+	#os.unlink(os.path.join( app.config['UPLOAD_FOLDER'] , str( spacenum )))
 
 @app.errorhandler(404)
 def new_page(error):
@@ -73,9 +82,10 @@ def new_page(error):
     	else:
         	return render_template('edit.html', page=None, pagepath=pagepath)
 	'''
-	return render_template('error.html')
 if __name__ == '__main__':
+	return render_template('error.html')
 	app.run(debug=True)
+
 	'''examples for gridfs functions
 	file_location = "/Users/bedrich/Desktop/TODO-MCI"
 	output_location = "/Users/bedrich/Desktop/omg"

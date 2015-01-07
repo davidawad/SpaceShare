b58ed68916509594e60cc528e6fa80b19943971a
David Awad <davidawad64@gmail.com>
2015-01-07 03:07:02 -0500

    adding Travis CI builds

---
+++ setup.py
@@ -0,0 +1,63 @@
+"""
+Flask-PyMongo
+-------------
+PyMongo support for Flask applications.
+Installation
+============
+Flask-PyMongo is pip-installable:
+    pip install Flask-PyMongo
+You can install the latest development snapshot like so:
+    pip install http://github.com/dcrosta/flask-pymongo/tarball/master#egg=Flask-PyMongo-dev
+Upgrading
+~~~~~~~~~
+- Version 0.2.0 introduced a dependency on PyMongo version 2.4 or later, and
+  introduced some potential backwards-breaking changes. Please review the
+  `Changelog <http://flask-pymongo.readthedocs.org/en/latest/#history-and-contributors>`_
+  carefully before upgrading.
+- Version 0.3.0 removed the `ReadPreference
+  <http://api.mongodb.org/python/current/api/pymongo/index.html#pymongo.read_preferences.ReadPreference>`_
+  redefinitions in ``flask_pymongo``, in favor of using the constants directly
+  from `PyMongo <http://api.mongodb.org/python/current/>`_. Please review the
+  `Changelog <http://flask-pymongo.readthedocs.org/en/latest/#history-and-contributors>`_
+  carefully before upgrading.
+Development
+===========
+Source code is hosted in `GitHub <https://github.com/dcrosta/flask-pymongo>`_
+(contributions are welcome!)
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='SpaceShare',
+    version='0.4',
+    url='spaceshare.me',
+    download_url='https://github.com/davidawad/spaceshare/',
+    license='BSD',
+    author='David Awad',
+    author_email='davidawad64@gmail.com',
+    description='Flask Applications using MongoDB',
+    long_description=__doc__,
+    zip_safe=False,
+    platforms='any',
+    packages=find_packages(),
+    install_requires=[
+        'Flask >= 0.8',
+        'pymongo >= 2.4',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    setup_requires=['nose'],
+    tests_require=['nose', 'coverage'],
+    test_suite='nose.collector',
+)

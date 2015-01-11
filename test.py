b00e9c9999d473693b0c5de6cf5897811a72c5aa
David Awad <Davidawad64@gmail.com>
2015-01-11 06:11:43 -0500

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ f.close()
 
 for x in range(0,2000):
   temp = math.rand()
-  __init__.read_file( uploads/example.txt , temp )
+  __init__.read_file( 'uploads/example.txt' , temp )
   __init__.search_file(temp )
   __init__.find_number( temp )
-  __init__.put_file('uploads/example.txt' temp )
+  __init__.put_file('uploads/example.txt', temp )

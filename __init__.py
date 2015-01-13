a7a226c8e005792a5a3195dd04a97a45b0b830f9
David Awad <davidawad64@gmail.com>
2015-01-13 01:21:49 -0500

    fixed unit testing functions, added useful debug information

--- __init__.py
+++ __init__.py
@@ -6,59 +6,32 @@ from random import randint
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'upload/'
 db = "spaceshare"
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
 # safety function to get a connection to the db above
 def get_db():
 	conn = None
 	try:
 	    conn = pymongo.MongoClient()
 	except pymongo.errors.ConnectionFailure, e:
-	   print "Could not connect to MongoDB: %s" % e
-	   sys.exit(1)
+	   raise Exception("Could not connect to MongoDB: %s" % e)
 	return conn[db]
 
-@app.route('/')
-def home():
-	return render_template('index.html')
-
-# put files in mongodb
-def put_file(file_name, room_number):
-	if not(file_name and room_number):
-		return
-	db_conn = get_db()
-	gfs = gridfs.GridFS(db_conn)
-	with open('upload/' + file_name, "r") as f:
-		gfs.put(f, room=room_number)
-
-# remove files from mongodb
-def delete_file(room_number):
-	if not(room_number):
-		return
-	db_conn = get_db()
-	gfs = gridfs.GridFS(db_conn)
-	_id = db_conn.fs.files.find_one(dict(room=room_number))['_id']
-	gfs.delete(_id)
-
-# read files from mongodb
-def read_file(output_location, room_number):
-	if not(output_location and room_number):
-		return
-	db_conn = get_db()
-	gfs = gridfs.GridFS(db_conn)
-	_id = db_conn.fs.files.find_one(dict(room=room_number))['_id']
-	with open(output_location + str(room_number) , 'w') as f:
-		f.write(gfs.get(_id).read())
-	gfs.get(_id).read()
-
-# find if files exist returns
+# returns if space is taken
 def search_file(room_number):
 	db_conn = get_db()
 	gfs = gridfs.GridFS(db_conn)
-	_id = db_conn.fs.files.find_one(dict(room = room_number))
+	try:
+		_id = db_conn.fs.files.find_one(dict(room = room_number))
+	except Exception:
+		_id = None
 	if not _id:
 		return False
 	else:
 		return True
-
 #find a random integer not currently in the db
 def find_number():
 	while True:
@@ -67,6 +40,54 @@ def find_number():
 			continue
 		else: ##we've found a random integer NOT already in the db, return
 			return temp
+
+# put files in mongodb
+def put_file(file_name, room_number):
+	if not(file_name and room_number):
+		return
+	db_conn = get_db()
+	gfs = gridfs.GridFS(db_conn)
+	if search_file(room_number):
+		print "space :"+str(room_number)+' is taken!'
+		return
+	try:
+		with open('upload/' + file_name, "r") as f:
+			gfs.put(f, room=room_number)
+		print "Stored file :"+room_number+' Successfully'
+	except Exception as e:
+		print "File :"+'upload/'+file_name+" probably doesn't exist, : "+str(e)
+
+# remove files from mongodb
+def delete_file(room_number):
+	if not(room_number):
+		return
+	if not search_file(room_number):
+		print "file "+str(room_number)+' not in db, error?'
+		return
+	db_conn = get_db()
+	gfs = gridfs.GridFS(db_conn)
+	_id = db_conn.fs.files.find_one(dict(room=room_number))['_id']
+	gfs.delete(_id)
+	print "Deleted file :"+room_number+' Successfully'
+
+# read files from mongodb
+def read_file(output_location, room_number):
+	if not(output_location and room_number):
+		return
+	if not search_file(room_number):
+		print "file "+str(room_number)+' not in db, error?'
+		return
+	db_conn = get_db()
+	gfs = gridfs.GridFS(db_conn)
+	try:
+		_id = db_conn.fs.files.find_one(dict(room=room_number))['_id']
+		with open('upload/' + str(room_number) , 'w') as f:
+			f.write(gfs.get(_id).read())
+		gfs.get(_id).read()
+		print "Written file :"+room_number+' Successfully'
+	except Exception as e:
+		print "failed to read file :"+str(e)
+		return False
 
 #upload routine
 @app.route('/upload',methods=['POST'])
@@ -108,6 +129,7 @@ def download(spacenum):
 @app.route('/get')
 def open_space():
 	space = request.form['space']
+	print "REDIRECT ON SERVER"
 	return redirect(url_for('/upload/'+str(space) ) )
 
 @app.errorhandler(404)
---
+++ unittest.py
@@ -0,0 +1,53 @@
+import __init__
+import os
+from random import randint, choice
+import subprocess
+import string
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(choice(chars) for _ in range(size))
+
+cmd = "rm -rf upload/"
+# no block, it starts as a sub process.
+p = subprocess.Popen(cmd , shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+# block until the cmd is done
+p.wait()
+
+if os.path.exists('upload/'):
+    raise Exception("SOMETHING WENT HORRIBLY WRONG. BREAKING.")
+
+os.makedirs('upload/')
+
+print id_generator()
+
+with open("upload/example.txt", "w+") as f:
+	f.write("this is some text I'm putting here.")
+
+for x in range(0,2000):
+	tName = id_generator()
+	tNum = randint(0,2000)
+## functions written in such a way that the upload folder is where the reads and writes will occur.
+	__init__.put_file(tName,tNum)
+	
+	__init__.search_file(tNum)
+
+
+
+for x in range(0,2000):
+  temp = randint(0,2000)
+  __init__.read_file('example.txt', temp)
+  # should raise exception of some sort on second insert.
+  __init__.read_file('example.txt', temp)
+  __init__.read_file('example.txt', temp)
+  __init__.search_file(temp)
+  unfound=__init__.find_number()
+  __init__.search_file(temp)
+  __init__.search_file(temp)
+  __init__.search_file(unfound)
+  __init__.put_file('example.txt', temp)
+  __init__.put_file('example.txt', unfound)
+  __init__.put_file('example.txt', temp)
+
+
+
+print 'Success'  

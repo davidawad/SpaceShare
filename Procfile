8ea7fa9609ef1e0620cfa4ab17e0ab418c3186bf
David Awad <davidawad64@gmail.com>
2015-01-14 20:21:36 -0500

    fixing procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn __init__:app
--- __init__.py
+++ __init__.py
@@ -3,6 +3,9 @@ from pymongo import MongoClient
 import os, gridfs, pymongo, time ##will add sendgrid and twilio functionality.
 from werkzeug import secure_filename
 from random import randint
+from werkzeug.contrib.fixers import ProxyFix
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app=Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'upload/'
 db = "share"
@@ -38,6 +41,7 @@ def search_file(room_number):
 		return False
 	else:
 		return True
+
 #find a random integer not currently in the db
 def find_number():
 	while True:
@@ -135,6 +139,7 @@ def upload():
 	def expire_file():
 		time.sleep(600)
 		delete_file(space)
+		os.unlink(os.path.join( app.config['UPLOAD_FOLDER'] , str( space )))
 		return
 
 @app.route('/upload/<spacenum>', methods=['GET'])

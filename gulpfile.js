cae8b69c3fba4b244dcfb8d0aa985e60b31b80dd
David Awad <dawad@rsglab.com>
2015-08-12 00:12:26 -0400

    gulpfile for dev

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,18 @@ var gulp = require('gulp'),
 // tasks
 
 gulp.task('transform', function () {
-  return gulp.src('./project/static/scripts/jsx/main.js')
+  return gulp.src('./spaceshare/static/jsx/app.js')
     .pipe(browserify({transform: ['reactify']}))
     .pipe(gulp.dest('./project/static/scripts/js'))
     .pipe(size());
 });
 
 gulp.task('clean', function () {
-  return gulp.src(['./project/static/scripts/js'], {read: false})
+  return gulp.src(['./spaceshare/static/scripts/js'], {read: false})
     .pipe(clean());
 });
 
 gulp.task('default', ['clean'], function () {
   gulp.start('transform');
-  gulp.watch('./project/static/scripts/jsx/main.js', ['transform']);
+  gulp.watch('./spaceshare/static/jsx/app.js', ['transform']);
 });

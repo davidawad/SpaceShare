6eb0379af95bb602e6d8c4ce96181b9f21f20ea5
David Awad <davidawad64@gmail.com>
2015-10-12 21:03:19 -0400

    fixing issue

--- spaceshare/config.py
+++ spaceshare/config.py
@@ -4,7 +4,7 @@ import yaml
 # The purpose of this file is to load the yaml file in memory and pass it
 # around to other module around.
 
-config_path = os.environ.get('FLEETDB_CONFIG')
+config_path = os.environ.get('some shell variable if you want')
 if config_path is None:
     config = yaml.load(open(os.path.dirname(__file__) + '/../settings.yaml'))
 else:

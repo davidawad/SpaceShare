63bf7b0aa6c555d6a50536e49f8fa36cbc3d0e46
David Awad <davidawad64@gmail.com>
2016-12-02 02:00:16 -0500

    webpack setup

---
+++ app/webpack.config.js
@@ -0,0 +1,30 @@
+/*
+ * David Awad
+ * ./node_modules/.bin/webpack -d
+ *
+ */
+
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'static/dist/');
+var APP_DIR = path.resolve(__dirname, 'static/jsx/');
+
+var config = {
+  entry: APP_DIR + '/shit.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test    : /\.jsx?/,
+        include : APP_DIR,
+        loader  : 'babel'
+      }
+    ]
+  }
+};
+
+module.exports = config;

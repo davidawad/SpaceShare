b14500bcd7872423814e2c4354c771cea7a2c2b3
David Awad <davidawad64@gmail.com>
2016-12-07 12:14:19 -0500

    webpack setup

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -11,7 +11,7 @@ var BUILD_DIR = path.resolve(__dirname, 'static/dist/');
 var APP_DIR = path.resolve(__dirname, 'static/jsx/');
 
 var config = {
-  entry: APP_DIR + '/shit.jsx',
+  entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
